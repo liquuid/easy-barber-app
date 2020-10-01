b75ce01249f536e4b855388debff82207bc44eaa
Fernando Henrique <liquuid@gmail.com>
2020-10-01 19:29:46 -0300

    signup screen

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
-import { Container } from './styles';
+import { 
+     Image,
+     View,
+     ScrollView,
+     KeyboardAvoidingView ,
+     Platform 
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { 
+     Container,
+     Title, 
+     BackToSignIn,
+     BackToSignInText 
+} from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+
+import logoImg from '../../assets/logo.png';
 
 const SignUp: React.FC = () => {
+    const navigation = useNavigation();
     return (
-        <Container />
+        <>
+        <KeyboardAvoidingView 
+            behavior={Platform.OS === 'ios' ? 'padding': undefined }
+            style={{flex: 1}}
+            enabled
+        >
+            <ScrollView 
+                contentContainerStyle={{ flex: 1 }}
+                keyboardShouldPersistTaps="handled"
+                >
+                <Container>
+                    <Image source={logoImg} />
+                    <View>
+                        <Title>Crie sua conta</Title>
+                    </View>
+                    <Input name="name" icon="user" placeholder="Nome" />
+                    <Input name="email" icon="mail" placeholder="Email" />
+                    <Input name="password" icon="lock" type="password" placeholder="Senha" />
+                    <Button onPress={() => { console.log('fooo')} }>Entrar</Button>
+
+                </Container>
+            </ScrollView>
+        </KeyboardAvoidingView>
+        <BackToSignIn onPress={() => navigation.goBack()}>
+            <Icon name="arrow-left" size={20} color="#fff" />
+            <BackToSignInText>
+                Voltar para logon
+            </BackToSignInText>
+        </BackToSignIn>
+        </>
     );
 
     
