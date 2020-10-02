88abe319c7643781c4599dfc35d17deec11a9e16
Fernando Henrique <liquuid@gmail.com>
2020-10-02 14:54:27 -0300

    form validation

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -68,7 +68,7 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({name, icon
     }, [registerField,fieldName])
 
     return (
-        <Container isFocused={isFocused}>
+        <Container isFocused={isFocused} isErrored={!!error}>
             <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'} />
             <TextInput 
                 ref={inputElementRef}
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -3,6 +3,7 @@ import FeatherIcon from 'react-native-vector-icons/Feather';
 
 interface ContainerProps {
     isFocused: boolean;
+    isErrored: boolean;
 }
 export const Container = styled.View<ContainerProps>`
     width: 100%;
@@ -15,9 +16,15 @@ export const Container = styled.View<ContainerProps>`
     border-color: #232129;
     flex-direction: row;
     align-items: center;
+
+    ${props => props.isErrored && css `
+        border-color: #c53030;
+    `}
+
     ${props => props.isFocused && css `
         border-color: #ff9000;
     `}
+
 `; 
 
 export const TextInput = styled.TextInput`
